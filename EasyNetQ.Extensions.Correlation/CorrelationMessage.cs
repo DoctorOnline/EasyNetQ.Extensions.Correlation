6e7c98095650661f58d315d3e9bb889c9e0f257a
Alexey Chuvagin <alexeychuvagin@me.com>
2019-10-22 23:52:07 +0300

    Added extension methods for IBus.

---
+++ EasyNetQ.Extensions.Correlation/CorrelationMessage.cs
@@ -0,0 +1,18 @@
+﻿namespace EasyNetQ.Extensions.Correlation
+{
+    /// <summary>
+    /// Class that contains information about correlation.
+    /// </summary>
+    internal sealed class CorrelationMessage<T>
+    {
+        /// <summary>
+        /// Unique correlation identifier.
+        /// </summary>
+        public string CorrelationId { get; set; }
+
+        /// <summary>
+        /// Body of a message.
+        /// </summary>
+        public T Body { get; set; }
+    }
+}
---
+++ EasyNetQ.Extensions.Correlation/Extensions.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Threading.Tasks;
+using EasyNetQ.FluentConfiguration;
+using Microsoft.Extensions.Logging;
+
+namespace EasyNetQ.Extensions.Correlation
+{
+    /// <summary>
+    /// Extensions for EasyNetQ for working with correlation across requests.
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Publish message to a message bus with a correlation identifier.
+        /// </summary>
+        public static Task PublishAsync<T>(this IBus messageBus, string correlationId, T body)
+        {
+            if (string.IsNullOrEmpty(correlationId))
+            {
+                throw new ArgumentException($"{nameof(correlationId)} cannot be null or empty.");
+            }
+
+            var message = new CorrelationMessage<T>
+            {
+                CorrelationId = correlationId,
+                Body = body
+            };
+
+            return messageBus.PublishAsync(message);
+        }
+
+        /// <summary>
+        /// Send message to a message bus with a correlation identifier.
+        /// </summary>
+        public static Task SendAsync<T>(this IBus messageBus, string queue, string correlationId, T body)
+        {
+            if (string.IsNullOrEmpty(correlationId))
+            {
+                throw new ArgumentException($"{nameof(correlationId)} cannot be null or empty.");
+            }
+
+            var message = new CorrelationMessage<T>
+            {
+                CorrelationId = correlationId,
+                Body = body
+            };
+
+            return messageBus.SendAsync(queue, message);
+        }
+
+        /// <summary>
+        /// Subscribe to get a message from a message bus with a correlation identifier.
+        /// </summary>
+        public static ISubscriptionResult SubscribeAsync<T>(
+            this IBus messageBus,
+            string subscriptionId,
+            Func<T, Task> onRecievedMessage,
+            Action<string> onRecievedCorrelationId,
+            Action<ISubscriptionConfiguration> configure,
+            ILogger logger)
+        {
+            return messageBus.SubscribeAsync<CorrelationMessage<T>>(
+                subscriptionId,
+                message => ReceiveMessageAsync(message, onRecievedMessage, onRecievedCorrelationId, logger),
+                configure
+            );
+        }
+
+        /// <summary>
+        /// Recieve a message from a message bus with a correlation identifier.
+        /// </summary>
+        public static IDisposable ReceiveAsync<T>(
+            this IBus messageBus,
+            string queue,
+            Func<T, Task> onRecievedMessage,
+            Action<string> onRecievedCorrelationId,
+            ILogger logger)
+        {
+            return messageBus.Receive<CorrelationMessage<T>>(
+                queue,
+                message => ReceiveMessageAsync(message, onRecievedMessage, onRecievedCorrelationId, logger)
+            );
+        }
+      
+        private static async Task ReceiveMessageAsync<T>(
+            CorrelationMessage<T> message,
+            Func<T, Task> onRecievedMessage,
+            Action<string> onRecievedCorrelationId,
+            ILogger logger)
+        {
+            onRecievedCorrelationId(message.CorrelationId);
+
+            // Put correlation identifier into a log context.
+            using (logger.BeginScope("{CorrelationId}", message.CorrelationId))
+            {
+                await onRecievedMessage(message.Body).ConfigureAwait(false);
+            }
+        }
+    }
+}
